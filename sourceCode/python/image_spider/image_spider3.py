1096136deedfa916573e8eef7d2e8e3730b67958
SinLogin <sin@SinLogindeMacBook-Pro.local>
2016-06-26 14:34:39 +0800

    change image_spider3.py

--- sourceCode/python/image_spider/image_spider3.py
+++ sourceCode/python/image_spider/image_spider3.py
@@ -7,22 +7,24 @@ Created on 2016年6月26日
 import re,urllib2
 
 url = "http://baozoumanhua.com/"
-image_type ="png"
+image_type_arr = ["png","jpg","jepg","tmp"]
 
 buf = urllib2.urlopen(url)
 
 urllist = re.findall(r"http://.+",buf.read())
 
-i=0
+for image_type in image_type_arr:
 
-for u in urllist:
-    try:
-        if image_type in u:
-            new_url = u[:u.find(image_type)+len(image_type)]
-            print ("crawl image %d:%s"%(i+1,new_url))
-            f = open(str(i)+"."+image_type,'w')
-            data = urllib2.urlopen(new_url)
-            f.write(data.read())
-            i += 1
-    except:
-        print ("crawl image failed:%s" %u)
+    i=0
+    
+    for u in urllist:
+        try:
+            if image_type in u:
+                new_url = u[:u.find(image_type)+len(image_type)]
+                print ("crawl image %d:%s"%(i+1,new_url))
+                f = open(str(i)+"."+image_type,'w')
+                data = urllib2.urlopen(new_url)
+                f.write(data.read())
+                i += 1
+        except:
+            print ("crawl image failed:%s" %u)
